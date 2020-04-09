234efe3b21b71e3dca1f3330ae1040fde5d7eca6
liajarysh <lyarysh@mail.ru>
2020-04-09 13:38:31 +0300

    Comments

---
+++ StrategyPattern/StrategyPattern/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public class Addition: ICalculateBehavior
+    {
+        public float Calculate(float a, float b)
+        {
+            Console.WriteLine("Сложение двух чисел");
+            return a + b;
+        }
+    }
+}
