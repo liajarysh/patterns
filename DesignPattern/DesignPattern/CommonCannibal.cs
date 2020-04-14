9c1ee30aa5da08b2aa776bfa0d0f574d8071dbf6
liajarysh <62939080+liajarysh@users.noreply.github.com>
2020-04-14 16:59:27 +0300

    Update CommonCannibal.cs

--- DesignPattern/DesignPattern/CommonCannibal.cs
+++ DesignPattern/DesignPattern/CommonCannibal.cs
@@ -8,7 +8,6 @@ namespace DesignPattern
 {
     class CommonCannibal: CannibalHabbits
     {
-        public override void chat() { }
         public override void cook()
         {
             Console.WriteLine("Сварил жертву");
